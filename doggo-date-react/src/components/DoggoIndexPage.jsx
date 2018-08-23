c005e5ff79b2d7d32711635a2f0a849b8b4bb9f1
Brian Coutts <bjhcoutts@gmail.com>
2018-08-23 11:26:25 -0700

    Doggo Filter Added

--- doggo-date-react/src/components/DoggoIndexPage.jsx
+++ doggo-date-react/src/components/DoggoIndexPage.jsx
@@ -12,7 +12,8 @@ class DoggoIndexPage extends Component {
     super(props);
 
     this.state = {
-      doggos: []
+      doggos: [],
+      value: "Crab Park"
     };
   }
 
@@ -23,6 +24,12 @@ class DoggoIndexPage extends Component {
       });
     });
   }
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
 
   render() {
     const { doggos } = this.state;
@@ -44,74 +51,76 @@ class DoggoIndexPage extends Component {
               <option value="" disabled selected hidden>
                 Please select a park...
               </option>
-              <option value="">Crab Park</option>
-              <option value="">Deer Lake</option>
-              <option value="">Dude Chilling</option>
-              <option value="">Hinge Park</option>
-              <option value="">Jericho</option>
-              <option value="">Jonathon Rogers</option>
-              <option value="">Kits Beach</option>
-              <option value="">Lighthouse Park</option>
-              <option value="">Pacific Spirit</option>
-              <option value="">Queen Elizabeth</option>
-              <option value="">Robson Square</option>
-              <option value="">Stanley Park</option>
-              <option value="">Sun Yat Sen</option>
-              <option value="">Trout Lake</option>
-              <option value="">Van Dusen</option>
+              <option value="Crab Park">Crab Park</option>
+              <option value="Deer Lake">Deer Lake</option>
+              <option value="Dude Chilling">Dude Chilling</option>
+              <option value="Hinge Park">Hinge Park</option>
+              <option value="Jericho">Jericho</option>
+              <option value="Jonathon Rogers">Jonathon Rogers</option>
+              <option value="Kits Beach">Kits Beach</option>
+              <option value="Lighthouse Park">Lighthouse Park</option>
+              <option value="Pacific Spirit">Pacific Spirit</option>
+              <option value="Queen Elizabeth">Queen Elizabeth</option>
+              <option value="Robson Square">Robson Square</option>
+              <option value="Stanley Park">Stanley Park</option>
+              <option value="Sun Yat Sen">Sun Yat Sen</option>
+              <option value="Trout Lake">Trout Lake</option>
+              <option value="Van Dusen">Van Dusen</option>
             </select>
           </form>
         </div>
 
         <ul style={{ padding: 0, listStyle: "none" }}>
-          {doggos.map((doggo, index) => (
-            <li className="index__doggo_container" key={doggo.id}>
-              {/* <div
+          {doggos
+            .filter(d => d.park === this.state.value)
+            .map((doggo, index) => (
+              <li className="index__doggo_container" key={doggo.id}>
+                {/* <div
                 className="index__doggo_img"
                 style={{
                   backgroundImage: `url(${DoggoImage.random.message})`
                 }}
               /> */}
-              <img src={poop} className="svg" />
-              <div className="index__doggo_card">
-                <Link to={`/doggo/index/${doggo.id}`}>
-                  <div
-                    className="index__doggo_img"
-                    style={{
-                      backgroundImage: `url(${process.env.PUBLIC_URL}${
-                        doggo.image
-                      })`
-                    }}
-                  />
-                </Link>
-                <div className="index__doggo_text">
-                  <div style={{ margin: "0 0 .25em 0" }}>
-                    <Link
-                      className="index__doggo_name"
-                      to={`/doggo/index/${doggo.id}`}
-                    >
-                      {doggo.name}
-                    </Link>
-                  </div>
+                <img src={poop} className="svg" />
+                <div className="index__doggo_card">
+                  <Link to={`/doggo/index/${doggo.id}`}>
+                    <div
+                      className="index__doggo_img"
+                      style={{
+                        backgroundImage: `url(${process.env.PUBLIC_URL}${
+                          doggo.image
+                        })`
+                      }}
+                    />
+                  </Link>
+                  <div className="index__doggo_text">
+                    <div style={{ margin: "0 0 .25em 0" }}>
+                      <Link
+                        className="index__doggo_name"
+                        to={`/doggo/index/${doggo.id}`}
+                      >
+                        {doggo.name}
+                      </Link>
+                    </div>
 
-                  <div className="index__doggo_subtitle">
-                    "{doggo.meme_phrase}"
-                  </div>
+                    <div className="index__doggo_subtitle">
+                      "{doggo.meme_phrase}"
+                    </div>
 
-                  <div className="index__doggo_details">
-                    <small>Favourite Park:</small>
-                    <br />
-                    <p style={{ fontSize: "1em" }}>{doggo.park}</p>
+                    <div className="index__doggo_details">
+                      <small>Favourite Park:</small>
+                      <br />
+                      <p style={{ fontSize: "1em" }}>{doggo.park}</p>
+                    </div>
+                    <small>
+                      Member since: <br />
+                      {new Date(doggo.created_at).toLocaleString()}
+                    </small>
                   </div>
-                  <small>
-                    Member since: <br />
-                    {new Date(doggo.created_at).toLocaleString()}
-                  </small>
                 </div>
-              </div>
-              <img src={bone} className="svg" />
-            </li>
-          ))}
+                <img src={bone} className="svg" />
+              </li>
+            ))}
         </ul>
         <div className="index__end">End of List</div>
         {/* <h1>Filtered List</h1>
