fb8a40ff963d1ed92239b6142db8e3ce18223606
Brian Coutts <bjhcoutts@gmail.com>
2018-09-06 17:36:14 -0700

    Pre Active Storage checkpoint

--- doggo-date-react/src/components/UserProfilePage.jsx
+++ doggo-date-react/src/components/UserProfilePage.jsx
@@ -13,6 +13,7 @@ class UserProfilePage extends Component {
           <h1>{this.props.currentUser.full_name}</h1>
         </div>
         <div className="baseDiv" style={{ margin: "1em" }}>
+          <h2>Doggos that this user belongs to:</h2>
           <ul>
             {this.props.currentUser.doggos.map((doggo, index) => (
               <li key={index}>{doggo.name}</li>
