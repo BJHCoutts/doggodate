c19d660dec26c74eed3591aecbfe8de658484849
Brian Coutts <bjhcoutts@gmail.com>
2018-09-19 11:44:58 -0700

    Doggo show page UI update

--- doggo-date-react/src/components/DoggoDetails.jsx
+++ doggo-date-react/src/components/DoggoDetails.jsx
@@ -17,24 +17,29 @@ const DoggoDetails = props => {
 
       <div className="doggo__details_container baseDiv">
         <h1>{props.name}</h1>
-        <div className="doggo__display_text">Breed:</div>
-        <div className="doggo__body_text">{props.breed}</div>
-        <div className="doggo__display_text">Sound</div>
-        <div className="doggo__body_text">{props.sound}</div>
-        <div className="doggo__display_text">Meme Phrase</div>
-        <div className="doggo__body_text">"{props.meme_phrase}"</div>
-        <div className="doggo__display_text">Age</div>
-        <div className="doggo__body_text">{props.age}</div>
-        <div className="doggo__display_text">Gender</div>
-        <div className="doggo__body_text">{props.gender}</div>
-        <div className="doggo__display_text">Coat Length</div>
-        <div className="doggo__body_text">{props.coat_length}</div>
-        <div className="doggo__display_text">Size</div>
-        <div className="doggo__body_text">{props.size}</div>
-        <div className="doggo__display_text">Category</div>
-        <div className="doggo__body_text">{props.category}</div>
-        <div className="doggo__display_text">Owner</div>
-        <div className="doggo__body_text">{props.user.first_name}</div>
+        <h2> "{props.meme_phrase}"</h2>
+        <div className="doggo__details_container_info">
+          <div className="col_one">
+            <div className="doggo__display_text">Breed</div>
+            <div className="doggo__body_text">{props.breed}</div>
+            <div className="doggo__display_text">Sound</div>
+            <div className="doggo__body_text">{props.sound}</div>
+            <div className="doggo__display_text">Age</div>
+            <div className="doggo__body_text">{props.age}</div>
+            <div className="doggo__display_text">Gender</div>
+            <div className="doggo__body_text">{props.gender}</div>
+          </div>
+          <div className="col_two">
+            <div className="doggo__display_text">Coat Length</div>
+            <div className="doggo__body_text">{props.coat_length}</div>
+            <div className="doggo__display_text">Size</div>
+            <div className="doggo__body_text">{props.size}</div>
+            <div className="doggo__display_text">Category</div>
+            <div className="doggo__body_text">{props.category}</div>
+            <div className="doggo__display_text">Owner</div>
+            <div className="doggo__body_text">{props.user.first_name}</div>
+          </div>
+        </div>
         <div className="doggo__display_text">Favourite Park</div>
         <div className="doggo__body_text">{props.park}</div>
       </div>
